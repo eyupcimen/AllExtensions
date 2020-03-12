6711745f642ce5cbeb7f3e9de572db625450d2ed
eyupcimen <cmneyp@gmail.com>
2020-03-12 11:05:34 +0300

    Initial Commit

---
+++ AllExtensions/AllExtensions.h
@@ -0,0 +1,19 @@
+//
+//  AllExtensions.h
+//  AllExtensions
+//
+//  Created by eyup cimen on 12.03.2020.
+//  Copyright © 2020 eyup cimen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AllExtensions.
+FOUNDATION_EXPORT double AllExtensionsVersionNumber;
+
+//! Project version string for AllExtensions.
+FOUNDATION_EXPORT const unsigned char AllExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AllExtensions/PublicHeader.h>
+
+
